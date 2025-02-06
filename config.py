13d97e238567755913e173c70906e4b18f2652e8
GokhanOfficial <32392391+GokhanOfficial@users.noreply.github.com>
2025-02-06 14:38:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+API_ID = 00000000  # Telegram API ID
+API_HASH = ""  # Telegram API Hash
+BOT_TOKEN = ""  # Telegram Bot Token
+
+ALLOWED_USERS = {0000000000,0000000000}  # Beyaz liste kullanıcılar
+LOG_CHANNEL_ID = -100   # Commands and upload log channel
+PROGRESS_UPDATE_INTERVAL = 7   # Refresh progress every 7 seconds
+EQUAL_SPLIT = False   # Equal splits over 2 GB
+
+YOUTUBE_API_KEY = ""    # Youtube Data Api V3 key for video search feature
+COOKIES_URL = ""   # Cookies.txt download url (optional)
+
+AV1_FOR_LOWRES = True  # AV1 enabled for 144p, 240p, 360p, 480p
+AV1_FOR_HIGHRES = True  # AV1 enabled for 720p, 1080p, 1440p, 2160p, 3840p

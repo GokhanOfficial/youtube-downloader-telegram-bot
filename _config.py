85700d87f75409eb3454db066e4194d5b99c3f78
GokhanOfficial <32392391+GokhanOfficial@users.noreply.github.com>
2025-02-08 02:31:55 +0300

    Update _config.py

--- _config.py
+++ _config.py
@@ -1,6 +1,7 @@
 API_ID = 00000000  # Telegram API ID
 API_HASH = ""  # Telegram API Hash
 BOT_TOKEN = ""  # Telegram Bot Token
+OWNER_ID = "" # Telegram Owner ID
 
 ALLOWED_USERS = {0000000000,0000000000}  # Beyaz liste kullanıcılar
 LOG_CHANNEL_ID = -100   # Commands and upload log channel
@@ -9,6 +10,7 @@ EQUAL_SPLIT = False   # Equal splits over 2 GB
 
 YOUTUBE_API_KEY = ""    # Youtube Data Api V3 key for video search feature
 COOKIES_URL = ""   # Cookies.txt download url (optional)
+PLACEHOLDER_AUDIO_URL = "" # Placeholder mp3 url (not necessary)
 
 AV1_FOR_LOWRES = True  # AV1 enabled for 144p, 240p, 360p, 480p
 AV1_FOR_HIGHRES = True  # AV1 enabled for 720p, 1080p, 1440p, 2160p, 3840p
